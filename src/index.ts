33cf8c2097089ca279990dd5a8ffe557b1ac5b47
凉凉 <xyqfer@gmail.com>
2019-10-03 21:56:22 +0800

    try furigana

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,41 @@ app.get("/", (req, res) => {
     `);
 });
 
+app.post('/furigana/translate-article', async (req, res) => {
+  const Kuroshiro = require('kuroshiro');
+  const KuromojiAnalyzer = require('kuroshiro-analyzer-kuromoji');
+  const cheerio = require('cheerio');
+
+  const { content } = req.body;
+
+    try {
+        const kuroshiro = new Kuroshiro();
+        await kuroshiro.init(new KuromojiAnalyzer());
+
+        const result = await kuroshiro.convert(content, { 
+            to: 'hiragana',
+            mode: 'furigana',
+        });
+        const $ = cheerio.load(`<div>${result}</div>`, {
+            decodeEntities: false,
+        });
+        $('ruby rp').remove();
+        const htmlContent = $('div').eq(0).html();
+
+        res.json({
+            success: true,
+            data: {
+                htmlContent,
+            },
+        });
+    } catch (err) {
+        console.error(err);
+        res.json({
+            success: false,
+        });
+    }
+});
+
 const reception = new Reception(
   PRSGame,
   APP_ID,
